fec51c90b99c85353564189b5b24c4553bc7ee88
Kojib <123880127+kojibai@users.noreply.github.com>
2026-01-12 15:21:15 -0500

    Export PortalView from PhiTerminal index

--- src/components/PhiTerminal/index.ts
+++ src/components/PhiTerminal/index.ts
@@ -1,5 +1,6 @@
 export { PhiTerminal } from "./PhiTerminal";
 export type { PhiTerminalProps } from "./PhiTerminal";
+export { PortalView } from "./views/PortalView";
 
 export * from "./protocol/types";
 export * from "./protocol/invoice";
